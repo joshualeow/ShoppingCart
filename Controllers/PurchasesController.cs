57490683a1f09de0a029b3ca3cf2c248d1d3febd
Jane A Lieviant <jlieviant@gmail.com>
2021-10-14 09:58:36 +0700

    merged with master, untested html

--- Controllers/PurchasesController.cs
+++ Controllers/PurchasesController.cs
@@ -12,69 +12,6 @@ namespace ShoppingCart.Controllers
 {
     public class PurchasesController : Controller
     {
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-            private DBContext dbContext;
-
-            public PurchasesController(DBContext dbContext)
-            {
-                this.dbContext = dbContext;
-            }
-            public IActionResult Index()
-            {
-                Session session = GetSession();
-                if (session == null)
-                {
-                    return RedirectToAction("Index", "Logout");
-                }
-                List<ShoppingCart.Models.Item> items = dbContext.Items.ToList();
-                ViewData["items"] = items;
-
-            List<ShoppingCart.Models.PurchasedItem> pitemslist = dbContext.PurchasedItems.ToList();
-            
-            
-
-
-
-            return View();
-            }
-
-            private Session GetSession()
-            {
-                if (Request.Cookies["SessionId"] == null)
-                {
-                    return null;
-                }
-
-                Guid sessionId = Guid.Parse(Request.Cookies["SessionId"]);// checking for the cookie,
-                                                                          // extract session and pass back
-                Session session = dbContext.Sessions.FirstOrDefault(x =>
-                    x.Id == sessionId
-                );
-
-                return session;//to use user information
-            }
-
-            private string GetPurchaseId([FromBody] Purchase purchase)
-            {
-                string purid = purchase.Id.ToString();
-                List<PurchasedItem> itemslist = dbContext.PurchasedItems.ToList();
-                IEnumerable<string> pur = 
-                    from i in itemslist
-                    select i.PurchaseId.ToString();
-
-                List<string> idlist = pur.ToList();
-                var returns = pur.Any(id => id == purid);
-                if (returns)
-                {
-                    string purID = purid;
-                    return purID;
-                }
-                return purid;
-            //Purchase purchase = dbContext.PurchasedItems.FirstOrDefault(x => x.Id == pur);
-
-            }
-=======
         private DBContext dbContext;
         public PurchasesController([FromServices] DBContext dbContext)
         {
@@ -82,15 +19,6 @@ namespace ShoppingCart.Controllers
         }
         public IActionResult MyPurchases()
         {
-=======
-        private DBContext dbContext;
-        public PurchasesController([FromServices] DBContext dbContext)
-        {
-            this.dbContext = dbContext;
-        }
-        public IActionResult MyPurchases()
-        {
->>>>>>> Stashed changes
             // redirect back to login page if session has expired or doesn't exist
             Session session = GetSession();
             if (session == null)
@@ -131,10 +59,5 @@ namespace ShoppingCart.Controllers
             Session session = dbContext.Sessions.FirstOrDefault(x => x.Id == sessionID);
             return session;
         }
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
     }
 }
-
