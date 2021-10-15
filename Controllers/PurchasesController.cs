8d522fee8eb3ec632f1d64565e83cbf06afc4fc3
Zhi Hao Joshua Leow <e0838403@u.nus.edu>
2021-10-15 20:28:51 +0800

    Added AcitvationKey

--- Controllers/PurchasesController.cs
+++ Controllers/PurchasesController.cs
@@ -15,47 +15,47 @@ namespace ShoppingCart.Controllers
         }
         public IActionResult MyPurchases()
         {
-            // // redirect back to login page if session has expired or doesn't exist
-            // Session session = GetSession();
-            // if (session == null)
-            //     return RedirectToAction("Index", "Login");
-            // // get all data:
-            // // item data from Items table,
-            // // Activation Key from PurchaseItems, and PurchaseDate from Purchases
-            // var purchaseList = dbContext.Purchases.Where(x => x.Userid == session.User.id).ToList();
-            // var purchasedItems = new Dictionary<Purchase, List<PurchasedItem>>();
-            // var ItemList = new List<Item>();
-            // foreach (Purchase purchase in purchaseList)
-            // {
-            //     var pItemList = dbContext.PurchasedItems.Where(x => x.PurchaseId == purchase.Id).ToList();
-            //     foreach (Guid ItemID in pItemList.Select(x => x.ItemId).Distinct())
-            //     {
-            //         ItemList.Add(dbContext.Items.FirstOrDefault(x => x.Id == ItemID));
-            //     }
-            // }
-            // var query = dbContext.Purchases
-            //     .Where(x => x.Userid == session.User.id)
-            //     .Join(dbContext.PurchasedItems,
-            //           purchase => purchase.Id, pItem => pItem.PurchaseId,
-            //           (purchase, pItem) => new
-            //           {
-            //               ActivationKey = pItem.ActivationKey,
-            //               ItemId = pItem.ItemId,
-            //               PurchaseDate = purchase.PurchaseDate
-            //           })
-            //     .Join(dbContext.Items,
-            //           pJoin => pJoin.ItemId, item => item.Id,
-            //           (pJoin, item) => new
-            //           {
-            //               ItemID = item.Id,
-            //               ActivationKey = pJoin.ActivationKey,
-            //               PurchaseDate = pJoin.PurchaseDate,
-            //               Name = item.Name,
-            //               Description = item.Description,
-            //               ImageURL = item.ImageUrl
-            //           })
-            //     .OrderByDescending(x => x.PurchaseDate);
-            // ViewData["query"] = query;
+            // redirect back to login page if session has expired or doesn't exist
+            Session session = GetSession();
+            if (session == null)
+                return RedirectToAction("Index", "Login");
+            // get all data:
+            // item data from Items table,
+            // Activation Key from PurchaseItems, and PurchaseDate from Purchases
+            var purchaseList = dbContext.Purchases.Where(x => x.Userid == session.User.id).ToList();
+            var purchasedItems = new Dictionary<Purchase, List<PurchasedItem>>();
+            var ItemList = new List<Item>();
+            foreach (Purchase purchase in purchaseList)
+            {
+                var pItemList = dbContext.PurchasedItems.Where(x => x.PurchaseId == purchase.Id).ToList();
+                foreach (Guid ItemID in pItemList.Select(x => x.ItemId).Distinct())
+                {
+                    ItemList.Add(dbContext.Items.FirstOrDefault(x => x.Id == ItemID));
+                }
+            }
+            var query = dbContext.Purchases
+                .Where(x => x.Userid == session.User.id)
+                .Join(dbContext.PurchasedItems,
+                      purchase => purchase.Id, pItem => pItem.PurchaseId,
+                      (purchase, pItem) => new
+                      {
+                          ActivationKey = CreateActivationKey(),
+                          ItemId = pItem.ItemId,
+                          PurchaseDate = purchase.PurchaseDate
+                      })
+                .Join(dbContext.Items,
+                      pJoin => pJoin.ItemId, item => item.Id,
+                      (pJoin, item) => new
+                      {
+                          ItemID = item.Id,
+                          ActivationKey = pJoin.ActivationKey,
+                          PurchaseDate = pJoin.PurchaseDate,
+                          Name = item.Name,
+                          Description = item.Description,
+                          ImageURL = item.ImageUrl
+                      })
+                .OrderByDescending(x => x.PurchaseDate);
+            ViewData["query"] = query;
             return View();
         }
         private Session GetSession()
@@ -65,6 +65,26 @@ namespace ShoppingCart.Controllers
             Guid sessionID = Guid.Parse(Request.Cookies["SessionID"]);
             Session session = dbContext.Sessions.FirstOrDefault(x => x.Id == sessionID);
             return session;
+        }
+        private string CreateActivationKey()
+        {
+            var activationKey = Guid.NewGuid().ToString();
+
+            List<PurchasedItem> item = dbContext.PurchasedItems.Where(x => x.ActivationKey == x.ActivationKey).ToList();
+            IEnumerable<string> iter =
+                from i in item
+                select i.ActivationKey;
+
+            List<string> keylist = iter.ToList();
+
+            var exists = keylist.Any(key => key == activationKey);
+
+            if (exists) //If there is a same one
+            {
+                activationKey = CreateActivationKey();
+            }
+
+            return activationKey;
         }
     }
 }
